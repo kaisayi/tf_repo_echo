188d24751b6561ddaf1d36eb00269c22be9d259c
obito <1209086740@qq.com>
2021-02-09 23:11:19 +0800

    add hovorod mnist

--- examples/tf_hovorod_mnist.py
+++ examples/tf_hovorod_mnist.py
@@ -2,4 +2,147 @@
 """
 datetime: 2021/2/6 22:16
 """
+from __future__ import division
 
+import os
+import errno
+import numpy as np
+import tensorflow as tf
+import horovod.tensorflow as hvd
+
+from tensorflow import keras
+
+tf.logging.set_verbosity(tf.logging.INFO)
+
+def cnn_model_fn(features, labels, mode):
+    """
+    build cnn model func
+    :param features:
+    :param labels:
+    :param mode:
+    :return:
+    """
+    input_layer = tf.reshape(features['x'], [-1, 28, 28, 1])
+
+    # conv layer1
+    conv1 = tf.layers.conv2d(
+        inputs=input_layer,
+        filters=32,
+        kernel_size=[5, 5],
+        padding="same",
+        activation=tf.nn.relu)
+    pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2, 2], strides=2)
+
+    conv2 = tf.layers.conv2d(
+        inputs=pool1,
+        filters=64,
+        kernel_size=[5, 5],
+        padding="same",
+        activation=tf.nn.relu)
+    pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2, 2], strides=2)
+    pool2_flat = tf.reshape(pool2, [-1, 7 * 7 * 64])
+
+    dense = tf.layers.dense(inputs=pool2_flat, units=1024,
+                            activation=tf.nn.relu)
+
+    dropout = tf.layers.dropout(
+        inputs=dense, rate=0.4, training=mode == tf.estimator.ModeKeys.TRAIN)
+
+    logits = tf.layers.dense(inputs=dropout, units=10)
+
+    predictions = {
+        # Generate predictions (for PREDICT and EVAL mode)
+        "classes": tf.argmax(input=logits, axis=1),
+        # Add `softmax_tensor` to the graph. It is used for PREDICT and by the
+        # `logging_hook`.
+        "probabilities": tf.nn.softmax(logits, name="softmax_tensor")
+    }
+
+    if mode == tf.estimator.ModeKeys.PREDICT:
+        return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions)
+
+    onehot_labels = tf.one_hot(indices=tf.cast(labels, tf.int32), depth=10)
+    loss = tf.losses.softmax_cross_entropy(
+        onehot_labels=onehot_labels, logits=logits)
+
+    if mode == tf.estimator.ModeKeys.TRAIN:
+        # Horovod: scale learning rate by the number of workers.
+        optimizer = tf.train.MomentumOptimizer(
+            learning_rate=0.001 * hvd.size(), momentum=0.9)
+        optimizer = hvd.DistributedOptimizer(optimizer, backward_passes_per_step=1)
+
+        train_op = optimizer.minimize(
+            loss=loss,
+            global_step=tf.train.get_global_step())
+        return tf.estimator.EstimatorSpec(mode=mode, loss=loss,
+                                          train_op=train_op)
+
+    eval_metric_ops = {
+        "accuracy": tf.metrics.accuracy(
+            labels=labels, predictions=predictions["classes"])}
+    return tf.estimator.EstimatorSpec(
+        mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
+
+
+def main():
+    hvd.init()
+
+    cache_dir = os.path.join(os.path.expanduser('~'), '.keras', 'datasets')
+    if not os.path.exists(cache_dir):
+        try:
+            os.mkdir(cache_dir)
+        except OSError as e:
+            if e.errno == errno.EEXIST and os.path.isdir(cache_dir):
+                pass
+            else:
+                raise
+    (train_data, train_labels), (eval_data, eval_labels) = keras.datasets.mnist.load_data('MNIST-data-%d' % hvd.rank())
+
+    train_data = np.reshape(train_data, (-1, 784)) / 255.0
+    eval_data = np.reshape(eval_data, (-1, 784)) / 255.0
+
+    # Horovod: pin GPU to be used to process local rank (one GPU per process)
+    config = tf.ConfigProto()
+    config.gpu_options.allow_growth = True
+    config.gpu_options.visible_device_list = str(hvd.local_rank())
+
+    # Horovod: save checkpoints only on worker 0 to prevent other workers from
+    # corrupting them.
+    model_dir = './mnist_convnet_model' if hvd.rank() == 0 else None
+
+    # Create the Estimator
+    mnist_classifier = tf.estimator.Estimator(
+        model_fn=cnn_model_fn, model_dir=model_dir,
+        config=tf.estimator.RunConfig(session_config=config))
+
+    tensors_to_log = {"probabilities": "softmax_tensor"}
+    logging_hook = tf.train.LoggingTensorHook(
+        tensors=tensors_to_log, every_n_iter=500)
+
+    bcast_hook = hvd.BroadcastGlobalVariablesHook(0)
+
+    train_input_fn = tf.estimator.inputs.numpy_input_fn(
+        x={"x": train_data},
+        y=train_labels,
+        batch_size=100,
+        num_epochs=None,
+        shuffle=True)
+
+    # Horovod: adjust number of steps based on number of GPUs.
+    mnist_classifier.train(
+        input_fn=train_input_fn,
+        steps=20000 // hvd.size(),
+        hooks=[logging_hook, bcast_hook])
+
+    # Evaluate the model and print results
+    eval_input_fn = tf.estimator.inputs.numpy_input_fn(
+        x={"x": eval_data},
+        y=eval_labels,
+        num_epochs=1,
+        shuffle=False)
+    eval_results = mnist_classifier.evaluate(input_fn=eval_input_fn)
+    print(eval_results)
+
+
+if __name__ == '__main__':
+    tf.app.run()
